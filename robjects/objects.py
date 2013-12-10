389c50de14d2986dea48efe54da3549bb7152c7f
Rolf Erik Lekang <me@rolflekang.com>
2013-12-10 16:54:34 +0100

    add HashObject
    HashObject is saved in a Redis hash. A Redis list is used to maintain a
    reference to all saved objects. Each object has its own Redis hash.

--- robjects/objects.py
+++ robjects/objects.py
@@ -27,3 +27,34 @@ class JsonObject(BaseObject):
     @classmethod
     def get(cls, key):
         return cls(**json.loads(cls.redis.hget(cls.HASH_KEY, key)))
+
+
+class HashObject(BaseObject):
+
+    def save(self):
+        if not self.key in self.redis.lrange(self.list_key(), 0, -1):
+            self.redis.lpush(self.list_key(), self.key)
+        return self.redis.hmset(self.HASH_KEY % self.key, self.__dict__)
+
+    def delete(self):
+        hash_key = self.HASH_KEY % self.key
+        self.redis.lrem(self.list_key(), self.key, 1)
+        for key in self.redis.hkeys(hash_key):
+            self.redis.hdel(hash_key, key)
+
+    @classmethod
+    def list_key(cls):
+        return cls.HASH_KEY % 'keylist'
+
+    @classmethod
+    def all(cls):
+        keys = cls.redis.lrange(cls.list_key(), 0, -1)
+        return [cls(**cls.get(key)) for key in keys]
+
+    @classmethod
+    def count(cls):
+        return len(cls.redis.lrange(cls.list_key(), 0, -1))
+
+    @classmethod
+    def get(cls, key):
+        return cls(**cls.redis.hgetall(cls.HASH_KEY % key))
